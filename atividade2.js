42bcbce6930ffcadd714ba595a635e24bef27b6f
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-01 15:20:43 -0300

    Add files via upload

---
+++ atividade2.js
@@ -0,0 +1,12 @@
+function questao1(numero, percentual){
+    var resultado = (numero / 100) * percentual;
+    console.log(resultado);
+  }
+  
+  function questao2(){
+    var array1 = ["Mariane"];
+    var array2 = ["Daniel"];
+    var novoArray = array1.concat(array2);
+    console.log(novoArray);
+    console.log(novoArray.join(" e "))
+  }
